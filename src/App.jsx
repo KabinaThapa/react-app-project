914e90316ae82d37f7c6950a36b7a26c9f331496
Kabina Thapa <thapakabina28@gmail.com>
2023-05-18 22:18:22 -0400

    JSX added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function App(){
+  return(
     <>
-    
-      <h1>Hello World!</h1>
-      
+    <nav>I am nav.</nav>
+    <header>I am header.</header>
+    <main>I am main.</main>
     </>
   )
 }
-
 export default App
