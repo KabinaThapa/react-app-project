1d9d4737bffd46cdf927a27dc552693af6ed25cb
Kabina Thapa <thapakabina28@gmail.com>
2023-05-19 00:41:15 -0400

    More css and HTML semantics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,16 @@ import './App.css'
 function App(){
   return(
     <>
-    <div>
+    <div class="bs">
     <nav>
       <h2>Navbar</h2>
       <ul>
-        <a href="#">
-        <li>Home</li>
-        </a>
-        <a href="#">
-        <li>About</li>
-        </a>
-        <a href="#">
-        <li>Contact</li>
-        </a>
         
+        <li> <a href="#"> Home</a></li>
+        <li> <a href="#"> About</a></li>
+        <li> <a href="#"> Contact</a></li>
+      
+         
       </ul>
 
     </nav>
@@ -24,7 +20,7 @@ function App(){
       <h2> Header</h2>
       <h3>Brand Logo</h3>
       </header>
-    
+    <div class="ms">
     <main>
       <h2>Main</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit id dolorem deleniti debitis, cumque praesentium ab sequi ut placeat odio, 
@@ -34,7 +30,10 @@ function App(){
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, sunt! Excepturi quae ut, rerum cumque omnis laborum laboriosam consequatur 
         et animi amet explicabo veniam maiores modi voluptas. Suscipit, voluptates nemo!</p>
     </section>
+    </div>
+    <div class="as">
     <article> 
+      <h2>Article</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id est, unde rerum illum earum officia non eligendi incidunt voluptatibus
          sunt deserunt sed nemo iusto neque fugit excepturi. Quo, assumenda nostrum?</p>
     </article>
@@ -42,6 +41,7 @@ function App(){
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe nesciunt numquam amet odio minus tempora doloribus modi, 
         in ex dicta repellendus aspernatur iure culpa pariatur nobis voluptatibus mollitia ratione eaque!</p>
     </aside>
+    </div>
     <footer> <h2>Footer</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium perferendis, eligendi sapiente culpa nostrum, vero natus eveniet quae architecto consequuntur, 
         illum veritatis nobis perspiciatis ipsum ex! Aperiam perspiciatis laborum temporibus.</p>
