a83199592ba253a4985029e154a9cb6591f6ac7d
Kabina Thapa <thapakabina28@gmail.com>
2023-05-18 23:41:14 -0400

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import './App.css'
+
 function App(){
   return(
     <>
-    <nav>I am nav.</nav>
-    <header>I am header.</header>
-    <main>I am main.</main>
+    <div>
+    <nav>
+      <h2>Navbar</h2>
+      <ul>
+        <a href="#">
+        <li>Home</li>
+        </a>
+        <a href="#">
+        <li>About</li>
+        </a>
+        <a href="#">
+        <li>Contact</li>
+        </a>
+        
+      </ul>
+
+    </nav>
+    <header>
+      <h2> Header</h2>
+      <h3>Brand Logo</h3>
+      </header>
+    
+    <main>
+      <h2>Main</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit id dolorem deleniti debitis, cumque praesentium ab sequi ut placeat odio, 
+        assumenda sunt quas cupiditate nobis dolores! Fugiat molestias ipsum repellendus.</p>
+    </main>
+    <section> <h2>Section</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, sunt! Excepturi quae ut, rerum cumque omnis laborum laboriosam consequatur 
+        et animi amet explicabo veniam maiores modi voluptas. Suscipit, voluptates nemo!</p>
+    </section>
+    <article> 
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id est, unde rerum illum earum officia non eligendi incidunt voluptatibus
+         sunt deserunt sed nemo iusto neque fugit excepturi. Quo, assumenda nostrum?</p>
+    </article>
+    <aside> <h2>Aside</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe nesciunt numquam amet odio minus tempora doloribus modi, 
+        in ex dicta repellendus aspernatur iure culpa pariatur nobis voluptatibus mollitia ratione eaque!</p>
+    </aside>
+    <footer> <h2>Footer</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium perferendis, eligendi sapiente culpa nostrum, vero natus eveniet quae architecto consequuntur, 
+        illum veritatis nobis perspiciatis ipsum ex! Aperiam perspiciatis laborum temporibus.</p>
+    </footer>
+    </div>
     </>
   )
 }
